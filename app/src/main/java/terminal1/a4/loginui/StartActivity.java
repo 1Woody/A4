f65af1a53ab5e11d18296f12686e0299ea4bcbf3
SergiSuarez <40240086+SergiSuarez@users.noreply.github.com>
2020-05-13 16:50:55 +0200

    Se cambia la pantalla inicial a Start Activity Como no hay id.json creado anteriormente redirige a la pantalla de login

--- app/src/main/java/terminal1/a4/loginui/StartActivity.java
+++ app/src/main/java/terminal1/a4/loginui/StartActivity.java
@@ -1,8 +1,15 @@
 package terminal1.a4.loginui;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.File;
+
+import terminal1.a4.listanegocios.nuevoperfil;
+
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        fichero();
     }
+
+    protected void fichero() {
+        File fichero = new File("id.json");
+
+        if (fichero.exists()){
+            Intent intent2 = new Intent(StartActivity.this, mis_vuelos.class);
+            startActivity(intent2);
+        }
+        else{
+            Intent intent1 = new Intent(StartActivity.this, Login.class);
+            startActivity(intent1);
+        }
+    }
+
 }
+
